cf89d047346b483cbda418f04376ec20011e8e15
Fykn <fernandokodagawa@gmail.com>
2026-07-26 17:41:10 -0300

    chore: modulariza e limpa index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,12 @@
 import express from 'express';
-import type { Tasks } from './models/taskModel.js';
+import { taskRoutes } from './routes/taskRoute.js';
 
 const app = express();
-
-const tasks: Tasks[] = [];
+const PORT = 3333;
 
 app.use(express.json());
+app.use(taskRoutes);
 
-app.post('/tasks', (req, res) => {
-  const newTask: Tasks = {
-    id: Math.floor(Math.random() * 100),
-    title: req.body.title,
-    description: req.body.description,
-    completed: req.body.completed
-  };
-  tasks.push(newTask);
-
-  return res.status(201).send({ message: 'Task enviada' });
-})
-
-app.get('/tasks', (req, res) => {
-  let result = tasks;
-
-  const completed = req.query.completed;
-  if (completed !== undefined) {
-    if (completed === "false") {
-      result = result.filter((task) => task.completed === false);
-    } else {
-      result = result.filter((task) => task.completed === true);
-    }
-  }
-
-  res.status(200).json(result);
-});
-
-app.get('/tasks/:id', (req, res) => {
-  const id: number = Number(req.params.id);
-  const task = tasks.find((e) => e.id === id);
-
-  if(!task) {
-    return res.status(404).json({ message: "404: Task not found" });
-  }
-
-  return res.json(task);
-})
-
-app.put('/tasks/:id', (req, res) => {
-  const id: number = Number(req.params.id);
-  const task = tasks.find((e) => e.id === id);
-
-  if(!task) {
-    return res.status(404).json({ message: "404: Task not found" });
-  }
-
-  task.title = req.body.title;
-  task.description = req.body.description;
-  task.completed = req.body.completed;
-
-  return res.status(200).send({ message: 'Task atualizada' });
-})
-
-app.delete('/tasks/:id', (req, res) => {
-  const id: number = Number(req.params.id);
-  const taskIndex = tasks.findIndex((e) => e.id === id);
-
-  if(taskIndex === -1) {
-    return res.status(404).json({ message: "404: Task not found" });
-  }
-
-  tasks.splice(taskIndex, 1);
-
-  return res.status(200).send({ message: 'Task deletada' });
-})
-
-app.listen(3000, () => {
-  console.log(`Servidor rodando na porta ${3000}`);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
